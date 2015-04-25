3a698099a3771b5715aecb926cadc2ca8d8f377b
Mikelin <linmxy@gmail.com>
2015-04-25 18:30:17 +0800

    add localstorage for flux store

--- src/scripts/stores/ContactStore.js
+++ src/scripts/stores/ContactStore.js
@@ -5,8 +5,9 @@ var util = require('../util/util');
 var __ = require('lodash');
 
 var CHANGE_EVENT = 'change';
+var LOCALSTORAGE_KEY = 'contacts-localCache';
 
-var _contacts = {};
+var _contacts = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)||'{}');
 
 /**
  * Create a Contact item.
@@ -16,6 +17,7 @@ function create(contact) {
   contact.id = util.oid();
   contact.avatar =  '/images/faces/' + Math.ceil(14 * Math.random()) + '.jpg';
   _contacts[contact.id] = contact;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(_contacts));
 }
 
 function update(id, updates) {
@@ -81,16 +83,43 @@ AppDispatcher.register(function(action) {
   }
 });
 
-[{'name': 'Test', 'tel': '324234234', 'email': 'asdf@dsf'},
-  {'name': 'Test', 'tel': '324234234', 'email': 'asdf@dsf'},
-  {'name': 'Test', 'tel': '324234234', 'email': 'asdf@dsf'},
-  {'name': 'Test', 'tel': '324234234', 'email': 'asdf@dsf'},
-].forEach(function (contact) {
-    AppDispatcher.dispatch({
-      actionType: ContactConstants.CONTACT_CREATE,
-      contact
+if(ContactStore.getAll().length<1){
+  [{
+    name : 'Terrence S. Hatfield',
+    tel: '651-603-1723',
+    email: 'TerrenceSHatfield@rhyta.com'
+  },
+    {
+      name : 'Chris M. Manning',
+      tel: '513-307-5859',
+      email: 'ChrisMManning@dayrep.com'
+    },
+    {
+      name : 'Ricky M. Digiacomo',
+      tel: '918-774-0199',
+      email: 'RickyMDigiacomo@teleworm.us'
+    },
+    {
+      name : 'Michael K. Bayne',
+      tel: '702-989-5145',
+      email: 'MichaelKBayne@rhyta.com'
+    },
+    {
+      name : 'John I. Wilson',
+      tel: '318-292-6700',
+      email: 'JohnIWilson@dayrep.com'
+    },
+    {
+      name : 'Rodolfo P. Robinett',
+      tel: '803-557-9815',
+      email: 'RodolfoPRobinett@jourrapide.com'
+    }
+  ].forEach(function (contact) {
+      AppDispatcher.dispatch({
+        actionType: ContactConstants.CONTACT_CREATE,
+        contact
+      });
     });
-  });
-
+}
 
 module.exports = ContactStore;
